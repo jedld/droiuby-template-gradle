775d230f35a7290bf6adb4d527b65615daa81fea
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2014-04-18 22:36:11 +0800

    Droiuby Template Gradle version

---
+++ init.rb
@@ -0,0 +1,26 @@
+require 'thor'
+
+class Init < Thor
+
+  include Thor::Actions
+
+  source_root File.join(File.dirname(__FILE__), 'templates')
+
+  desc "init [java package] [archive] [name]", "initialize this template project"
+  method_option :hybrid, :type => :boolean, :default=>false
+
+  def init(package, archive, name = 'HelloWorld')
+    package_folders = package.split('.')
+    dest_folder = File.join('app','src','main')
+    @package_name = package
+    @archive_name = archive
+    @project_name = name
+    template File.join('AndroidManifest.xml.erb'), File.join(dest_folder, "AndroidManifest.xml")
+    template File.join('strings.xml.erb'), File.join(dest_folder, 'res','values','strings.xml')
+    if (options[:hybrid])
+      template File.join('index.xml.erb'), File.join(dest_folder, 'res','layout','index.xml')
+    end
+    template File.join('StartupActivity.java.erb'), File.join(dest_folder, 'java', *package_folders, "StartupActivity.java")
+    template File.join('DroiubyActivity.java.erb'), File.join(dest_folder, 'java', *package_folders, "DroiubyActivity.java")
+  end
+end
